2bc0d63a9180b9075e26e3843b112d8d3471e609
mayurwaghmarepatil <122507311+mayurwaghmarepatil@users.noreply.github.com>
2023-01-23 16:26:55 +0530

    Add files via upload

---
+++ SecondProblem.java
@@ -0,0 +1,12 @@
+public class SecondProblem {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int num = 1; num <= n-1; num++) {
+            for (int i = 1; i < n; i++) {
+                System.out.print(num);
+            }
+            System.out.println();
+
+        }
+    }
+}
