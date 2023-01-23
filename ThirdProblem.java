38553aa571fe3872b7545a56aef7ebf00f43216b
mayurwaghmarepatil <122507311+mayurwaghmarepatil@users.noreply.github.com>
2023-01-23 17:21:31 +0530

    Add files via upload

---
+++ ThirdProblem.java
@@ -0,0 +1,15 @@
+public class ThirdProblem {
+    public static void main(String[] args) {
+     int n=20;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if((i+j<=(n-1)/2) || (j-i>=(n-1)/2) || j==0 ||i==0 || i==n-1 || j==n-1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
